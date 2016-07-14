e6ea9bc3f0a6850ae15182f10a0d2199b7f707c6
Dawson Botsford <3408480+dawsbot@users.noreply.github.com>
2016-07-13 21:33:10 -0700

    remove network checking entirely

--- test/index.js
+++ test/index.js
@@ -2,46 +2,31 @@ const chai = require('chai');
 const assert = chai.assert;
 const gmap = require('../dist');
 const _constants = require('../dist/constants');
-const http = require('http-request');
-
-// check if a url's http code is 200
-const is200 = function (url, done) {
-  http.get(url, function (error, response) {
-    if (!error && response.code === 200) {
-      done();
-    } else {
-      done(new Error('err: ' + error + '- http code: ' + response.statusCode));
-    }
-  });
-};
 
 describe('gmap', function () {
-  it('empty object query', function (done) {
+  it('empty object query', function () {
     const query = {};
     assert.equal(gmap(), _constants.baseUri);
     assert.equal(gmap(query), _constants.baseUri);
-    is200(gmap.directions(query), done);
   });
 
-  it('simple object query', function (done) {
+  it('simple object query', function () {
     const query = {
       saddr: 'here'
     };
     assert.equal(gmap(query), _constants.baseUri + '?saddr=here');
-    is200(gmap.directions(query), done);
   });
 
-  it('complex object query', function (done) {
+  it('complex object query', function () {
     const query = {
       saddr: 'here',
       daddr: 'there',
       dirflg: 'r'
     };
     assert.equal(gmap(query), _constants.baseUri + '?saddr=here&daddr=there&dirflg=r');
-    is200(gmap(query), done);
   });
 
-  it('complex object query with array daddr', function (done) {
+  it('complex object query with array daddr', function () {
     const query = {
       saddr: 'here',
       daddr: [
@@ -51,22 +36,19 @@ describe('gmap', function () {
       ]
     };
     assert.match(gmap(query), /daddr=dest%201\+to:dest2\+to:dest3/, 'regexp matches');
-    is200(gmap(query), done);
   });
 });
 
 describe('gmap.directions', function () {
-  it('basic from and to', function (done) {
+  it('basic from and to', function () {
     assert.equal(gmap.directions('here', 'there'), _constants.baseUri + '?saddr=here&daddr=there');
-    is200(gmap.directions('here', 'there'), done);
   });
 
-  it('basic from and to with query', function (done) {
+  it('basic from and to with query', function () {
     const query = {
       dirflg: 'r'
     };
 
     assert.match(gmap.directions('here', 'there', query), /dirflg=r/, 'regexp matches');
-    is200(gmap.directions('here', 'there'), done);
   });
 });
